6565d36dd37abd51d22a75ae459ef3f5232d9b3d
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-09-05 08:16:51 +0200

    init commit

---
+++ src/ait/employee/dao/Company.java
@@ -0,0 +1,26 @@
+package ait.employee.dao;
+
+import ait.employee.model.Employee;
+
+public interface Company {
+    String TITLE = "Apple";
+    boolean addEmployee(Employee employee);
+
+    Employee removeEmployee(int id);
+
+    Employee findEmployee(int id);
+    int quantity();
+    double totalSalary();
+    default double avgSalary(){
+        return totalSalary()/quantity();
+    }
+
+    double totalSales();
+
+    void printEmployees();
+
+    Employee[] findEmployeesHoursGreaterThan(int hours);
+
+    Employee[] findEmployeeSalaryRange(int minSalary, int maxSalary);
+
+}
---
+++ src/ait/generic/JsonWrapper1Appl.java
@@ -0,0 +1,13 @@
+package ait.generic;
+
+import ait.generic.util.JsonWrapper1;
+
+public class JsonWrapper1Appl {
+    public static void main(String[] args) {
+        JsonWrapper1 wrapper1 = new JsonWrapper1(10);
+        System.out.println(wrapper1);
+        Integer a = (Integer) wrapper1.getValue();
+        System.out.println(a);
+    }
+
+}
